cae972c623d1a35d41ca7344323e85a2efb1e31a
fluffy29 <hmajzoub12@gmail.com>
2024-04-23 14:43:28 +0200

    This part of the commit is for the if else

--- if_else.php
+++ if_else.php
@@ -1,12 +1,21 @@
-<?php
-$grade = 'B';
-switch ($grade) {
-    case 'A';
-        echo 'Excellent!';
-        break;
-    case 'B';
-        echo 'Good job';
-        break;
-    default:
-           echo 'Good job better luck next time';
-}
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Title</title>
+</head>
+
+<body>
+    <?php
+    $something = 11;
+    if ($something > 10) {
+        echo "The variable is greater than 10";
+    } else {
+        echo "the variable is less than 10";
+    }
+    ?>
+</body>
+
+</html>
