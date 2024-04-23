5794652590cde76745d917db9b2e94d5c655b37b
fluffy29 <hmajzoub12@gmail.com>
2024-04-23 14:26:05 +0200

    If else in PHP

---
+++ if_else.php
@@ -0,0 +1,12 @@
+<?php
+$grade = 'B';
+switch ($grade) {
+    case 'A';
+        echo 'Excellent!';
+        break;
+    case 'B';
+        echo 'Good job';
+        break;
+    default:
+           echo 'Good job better luck next time';
+}
