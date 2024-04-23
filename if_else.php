dc47ef06d0b0edf850da40ec69623dd0ae71e9c5
fluffy29 <hmajzoub12@gmail.com>
2024-04-23 14:56:53 +0200

    while and if example

--- if_else.php
+++ if_else.php
@@ -2,25 +2,25 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Title</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Title</title>
 </head>
 
 <body>
-    <?php 
-    $i = 0;
-    while ($i< 6) {
-        $i++;
-        if ($i==3) {
-            continue;
-        }
-        if ($i=5) {
-            break;
-        }
-        echo $i. '<br>';
+  <?php
+  $i = 2;
+  do {
+    $i++;
+    if ($i == 3) {
+      continue; # Skip to the next iteration
     }
-    ?>
+    if ($i == 5) {
+      break; # Exit the loop
+    }
+    echo $i; # Only print numbers between 4 and (excluding) 5
+  } while ($i < 10);
+  ?>
 </body>
 
 </html>
