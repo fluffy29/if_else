20b63c69c9bf598e7a803197541ad4e2570f3c06
fluffy29 <hmajzoub12@gmail.com>
2024-04-23 17:19:40 +0200

    $_POST is a PHP super global variable which is used to collect form data after submitting an HTML form with method="post". $_POST is also widely  used to pass variables.
     and also it makes a validate button

--- if_else.php
+++ if_else.php
@@ -1,34 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
+<html>
+    <head> </head>
+    <body>
+        <form method="post" action="my action.php">
+            Name: <input type="text" name="name">
+            <input type="submit" value="Validate">
+        </form>
+    </body>
+</html>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Title</title>
-</head>
-
-<body>
-<?php
-
-$inventory = array(
-    "printer" => array(
-      "stock" => 10,
-      "sold" => 15,
-    ),
-    "screens" => array(
-      "stock" => 20,
-      "sold" => 5,
-    ),
-    "keyboard" => array(
-      "stock" => 25,
-      "sold" => 10,
-    ),
-  );
-
-foreach ($inventory as $item => $details) {
-    $stock = $details['stock'];
-    $sold = $details['sold'];
-    echo "$item: Stock: $stock, Sold: $sold <br>";
-}
-  
-?>
